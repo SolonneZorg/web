5f26bc66f640870dd2655b9ef78a2fa6f89a5db0
MoonTech168 <mtbasesix@gmail.com>
2023-06-18 00:23:28 +0200

    update styles.css

--- scripts/build-pug.js
+++ scripts/build-pug.js
@@ -1,3 +1,4 @@
+
 'use strict';
 const upath = require('upath');
 const sh = require('shelljs');
@@ -6,7 +7,7 @@ const renderPug = require('./render-pug');
 const srcPath = upath.resolve(upath.dirname(__filename), '../src');
 
 sh.find(srcPath).forEach(_processFile);
-
+//
 function _processFile(filePath) {
     if (
         filePath.match(/\.pug$/)
