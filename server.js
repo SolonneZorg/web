60cae23d726ee2278fd091d9e4f83be24750e374
MoonTech168 <mtbasesix@gmail.com>
2023-06-18 02:08:37 +0200

    serve static files

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+
+const staticPath = path.join(__dirname, 'dist');
+app.use(express.static(staticPath));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(staticPath, 'index.html'));
 });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
